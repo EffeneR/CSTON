a0574d8f5586e8bcebec150bb609a00975d82320
EffeneR <125957025+EffeneR@users.noreply.github.com>
2024-12-12 15:42:32 +0400

    Update manager.js

--- models/manager.js
+++ models/manager.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const ManagerSchema = new mongoose.Schema({
-    telegramId: { type: String, required: true, unique: true }, // Unique identifier for Telegram user
-    username: { type: String, required: true }, // Telegram username
-    createdAt: { type: Date, default: Date.now }, // Registration time
-    lastLogin: { type: Date, default: Date.now }, // Last active timestamp
+const managerSchema = new mongoose.Schema({
+  telegramId: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  teamName: { type: String, required: true },
+  teamNationality: { type: String, required: true },
 });
 
-module.exports = mongoose.model('Manager', ManagerSchema);
+module.exports = mongoose.model('Manager', managerSchema);
