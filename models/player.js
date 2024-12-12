1511747018c9a26e1fcecebcb23b94266fa8dae0
EffeneR <125957025+EffeneR@users.noreply.github.com>
2024-12-12 15:57:30 +0400

    Update player.js

--- models/player.js
+++ models/player.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const playerSchema = new mongoose.Schema({
-  managerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },
-  name: { type: String, required: true },
-  nationality: { type: String, required: true },
-  skill: { type: Number, required: true },
+    name: { type: String, required: true },
+    skill: { type: Number, required: true },
+    nationality: { type: String, required: true },
+    teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },
 });
 
 module.exports = mongoose.model('Player', playerSchema);
