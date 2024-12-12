c4a586900f53078f8c741a58c0054d49765cf6c1
EffeneR <125957025+EffeneR@users.noreply.github.com>
2024-12-12 15:57:21 +0400

    Update manager.js

--- models/manager.js
+++ models/manager.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const managerSchema = new mongoose.Schema({
-  telegramId: { type: String, required: true, unique: true },
-  username: { type: String, required: true },
-  teamName: { type: String, required: true },
-  teamNationality: { type: String, required: true },
+    telegramId: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    teamName: { type: String, required: true },
+    nationality: { type: String, default: 'International' },
 });
 
 module.exports = mongoose.model('Manager', managerSchema);
