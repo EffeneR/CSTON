a49e0469c672a1331fb3967e65c0647c9880fad5
EffeneR <125957025+EffeneR@users.noreply.github.com>
2024-12-12 15:42:20 +0400

    Update player.js

--- models/player.js
+++ models/player.js
@@ -1,17 +1,10 @@
 const mongoose = require('mongoose');
 
-const PlayerSchema = new mongoose.Schema({
-    name: { type: String, required: true }, // Player name
-    nationality: { type: String, required: true }, // Player's nationality
-    role: { type: String, enum: ['AWPer', 'Rifler', 'Support', 'IGL'], required: true }, // Player role
-    skill: {
-        accuracy: { type: Number, default: 50 }, // Skill: Accuracy (0-100)
-        strategy: { type: Number, default: 50 }, // Skill: Strategy (0-100)
-        reaction: { type: Number, default: 50 }, // Skill: Reaction (0-100)
-    },
-    value: { type: Number, default: 1000 }, // Player market value
-    age: { type: Number, default: 20 }, // Player age
-    teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Team' }, // Link to the team
+const playerSchema = new mongoose.Schema({
+  managerId: { type: mongoose.Schema.Types.ObjectId, ref: 'Manager' },
+  name: { type: String, required: true },
+  nationality: { type: String, required: true },
+  skill: { type: Number, required: true },
 });
 
-module.exports = mongoose.model('Player', PlayerSchema);
+module.exports = mongoose.model('Player', playerSchema);
